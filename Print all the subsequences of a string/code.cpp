48db77e149eafb8a012ddf5374c3f74c65d4925d
himanshu-red <45516142+himanshu-red@users.noreply.github.com>
2021-12-30 09:39:30 +0530

    Create code.cpp

---
+++ Print all the subsequences of a string/code.cpp
@@ -0,0 +1,16 @@
+int main()
+{
+    string str; 
+    cin >> str; 
+    int noOfSubs = pow(2, str.size()); 
+    for(int i = 1; i< noOfSubs; i++)
+    {
+        int firstSetBit = (int)log2(i);
+        for (int j = 0; j <= firstSetBit; j++)
+        {
+            if (i & (1 << j))
+                cout << str[j]; 
+        }
+        cout << endl;
+    }
+}
