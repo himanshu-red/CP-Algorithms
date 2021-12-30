6e120886dd2523976bd18cf510f26223230a3f92
himanshu-red <45516142+himanshu-red@users.noreply.github.com>
2021-12-30 09:44:26 +0530

    Update code.cpp

--- Print all the subsequences of a string using power set/code.cpp
+++ Print all the subsequences of a string using power set/code.cpp
@@ -5,7 +5,7 @@ int main()
     int noOfSubs = pow(2, str.size()); 
     for(int i = 1; i< noOfSubs; i++)
     {
-        int firstSetBit = (int)log2(i);
+        int firstSetBit = (int)log2(i);     // finds out the rightmost set bit
         for (int j = 0; j <= firstSetBit; j++)
         {
             if (i & (1 << j))
