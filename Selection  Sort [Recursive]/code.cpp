28724303a16da4dbd73f82e14cc03a31bd07662b
HIMANSHU RANJAN <45516142+himanshu-red@users.noreply.github.com>
2022-01-13 15:35:14 +0530

    Create code.cpp

---
+++ Selection  Sort [Recursive]/code.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std; 
+int getIndex(vector<int> &v, int largest, int index)
+{
+    if (index == v.size()) return largest; 
+    if (v[largest] > v[index])
+        largest = index; 
+    return getIndex(v, largest, index+1); 
+}
+void selectionSort(vector<int> &v, int start)
+{
+    if (start == v.size()-1) return;
+    int index = getIndex(v, start, start); 
+    int temp = v[index];
+    v[index] = v[start]; 
+    v[start] = temp; 
+    return selectionSort(v, start+1); 
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &i : v)
+        cin >> i;
+    selectionSort(v, 0);
+    for (auto &i : v)
+        cout << i << " ";
+}
