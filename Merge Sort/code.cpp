efedee27425180d2cc8baee69780121f5f2a4de8
HIMANSHU RANJAN <45516142+himanshu-red@users.noreply.github.com>
2022-01-17 10:27:35 +0530

    Update code.cpp

--- Merge Sort/code.cpp
+++ Merge Sort/code.cpp
@@ -8,27 +8,26 @@ void merge(vector<int> &v, int s1, int e1, int s2, int e2)
     int i1 = s1;
     int i2 = s2;
 
-    while (s1 <= e1 && s2 <= e2)
+    while (s1 <= e1 || s2 <= e2)
     {
-        if (v[s1] >= v[s2])     sorted.push_back(v[s2++]);
-        else                    sorted.push_back(v[s1++]);
+        if (s2 > e2 || (s1 <= e1 && v[s1] < v[s2])) sorted.push_back(v[s1++]); 
+        else                                        sorted.push_back(v[s2++]); 
     }
-
-    while (s1 <= e1)    sorted.push_back(v[s1++]);
-    while (s2 <= e2)    sorted.push_back(v[s2++]);
 
     int j = 0;
 
-    for (int i = i1; i <= e1; i++)     v[i] = sorted[j++];
-    for (int i = i2; i <= e2; i++)     v[i] = sorted[j++];
+    for (int i = i1; i <= e1; i++)       v[i] = sorted[j++];
+    for (int i = i2; i <= e2; i++)       v[i] = sorted[j++];
 }
 
 void mergeSort(vector<int> &v, int s, int e)
 {
     int mid = s + (e - s) / 2;
-    
-    if (v.size() == 0) return; 
-    if (s == e)  return;
+
+    if (v.size() == 0)
+        return;
+    if (s == e)
+        return;
 
     mergeSort(v, s, mid);
     mergeSort(v, mid + 1, e);
